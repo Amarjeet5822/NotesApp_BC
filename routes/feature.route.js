d5027df699b1f5bb32552d4444bed6cf4dd4cb12
Amarjeet5822 <amar.bst5822@gmail.com>
2025-02-09 11:23:02 +0530

    feature to sort notes and filter by category

--- routes/feature.route.js
+++ routes/feature.route.js
@@ -34,10 +34,40 @@ featuresRouter.get("/filter", async (req, res) => {
     }
     const filteredNotes = await NoteModel.find({ priority : priority })
     res.status(200).json(filteredNotes);
-    
+
   } catch (error) {
     res.status(500).json({ msg: "Server error", error });
   }
 });
 
+// filter notes by Category
+featuresRouter.get("/filter-category", async (req, res) => {
+  try {
+    // http://localhost:8080/filter-category?category="category"
+    const { category } = req.query; 
+    if (!category) {
+      return res.status(400).json({ msg: "Category is required for filtering" });
+    }
+    const filteredNotes = await Note.find({ category: category });
+    res.status(200).json(filteredNotes);
+
+  } catch (error) {
+    res.status(500).json({ msg: "Server error", error });
+  }
+});
+
+featuresRouter.get("/sorted", async (req, res) => {
+  try {
+    // GET http://localhost:8080/notes/sorted?sortBy=title&order=asc
+    let { sortBy, order } = req.query;
+    // Default sorting if not provided
+    sortBy = sortBy || "createdAt"; // Default: Sort by creation date
+    order = order === "desc" ? -1 : 1; // Default: Ascending (1)
+
+    const sortedNotes = await Note.find().sort({ [sortBy]: order });
+    res.status(200).json(sortedNotes);
+  } catch (error) {
+    res.status(500).json({ msg: "Server error", error });
+  }
+});
 module.exports = { featuresRouter };
