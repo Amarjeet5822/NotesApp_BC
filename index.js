efe373f422bc809e559965cef758d0e929c0e9aa
Amarjeet5822 <amar.bst5822@gmail.com>
2025-02-28 09:27:11 +0530

    modified cors using npm cors

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { connectDB } = require("./dbConnection");
-const swaggerDocs = require("./swaggerConfig") // import swaggerDocs
+const swaggerDocs = require("./swaggerConfig"); // import swaggerDocs
 require("dotenv").config();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
@@ -14,24 +14,25 @@ app.use(express.urlencoded({ extended: false }));
 const cookieParserSecret = process.env.SECRET_KEY;
 app.use(cookieParser(cookieParserSecret));
 
-app.use(cors({
-  origin: (origin, callback) => {
-    const allowedOrigins = [process.env.FE_URL, process.env.DEPLOY_FE_URL];
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true); // Allow requests from valid origins or Postman (no origin)
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-  methods: ["GET", "HEAD", "PATCH", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
-}));
+const whitelist = [process.env.FE_URL, process.env.DEPLOY_FE_URL];
+
+const corsOptionsDelegate = (req, callback) => {
+  if (whitelist.indexOf(req.header("Origin")) !== -1) {
+    callback(null, {
+      origin: req.header("Origin"), //// Automatically reflects the request's origin if in the whitelist
+      credentials: true,
+      methods: "GET,HEAD,PATCH,POST,PUT,DELETE",
+      allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+    }); // reflect (enable) the requested origin in the CORS response
+  } else {
+    callback(null, {origin: false}); // Deny CORS if not in whitelist
+  }
+};
+app.use(cors(corsOptionsDelegate));
 
 app.use(router);
 
 swaggerDocs(app); // Load Swagger for Documentation
-
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
